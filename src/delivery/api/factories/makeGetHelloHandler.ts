1057b118edb00d0e481562fdcb357439d09f8b31
Alex Cristea <alexcristea@me.com>
2021-11-08 23:14:59 +0200

    Integrate InMemoryGreetingsRepo within the factories

--- src/delivery/api/factories/makeGetHelloHandler.ts
+++ src/delivery/api/factories/makeGetHelloHandler.ts
@@ -1,6 +1,7 @@
 import { makeLogger, withLogging } from '../../../shared/logger'
 
 import { HandlerFactory } from '../types'
+import { InMemoryGreetingsRepo } from '../../../repository/InMemoryGreetingsRepo'
 import { getHelloHandler } from '../handlers'
 import { makeContextFromRequest } from '../utils'
 import { sayHelloUseCase } from '../../../domain/usecases'
@@ -9,7 +10,9 @@ export const makeGetHelloHandler: HandlerFactory = (req, res) => {
   const context = makeContextFromRequest(req)
   const logger = makeLogger({ context })
 
-  const usecase = sayHelloUseCase({})
+  const repo = InMemoryGreetingsRepo()
+
+  const usecase = sayHelloUseCase({ repo })
   const decoratedUsecase = withLogging(logger, 'USECASE', 'sayHelloUseCase')(usecase)
 
   const handler = getHelloHandler({
--- src/delivery/lamdba/factories/makeHelloHandler.ts
+++ src/delivery/lamdba/factories/makeHelloHandler.ts
@@ -1,6 +1,7 @@
 import { makeContext, makeLogger, withLogging } from '../../../shared/logger'
 
 import { HandlerFactory } from '../types'
+import { InMemoryGreetingsRepo } from '../../../repository/InMemoryGreetingsRepo'
 import { getHelloHandler } from '../handlers'
 import { sayHelloUseCase } from '../../../domain/usecases'
 
@@ -8,7 +9,9 @@ export const makeGetHelloHandler: HandlerFactory = (event, _context) => {
   const context = makeContext()
   const logger = makeLogger({ context })
 
-  const usecase = sayHelloUseCase({})
+  const repo = InMemoryGreetingsRepo()
+
+  const usecase = sayHelloUseCase({ repo })
   const decoratedUsecase = withLogging(logger, 'USECASE', 'sayHelloUseCase')(usecase)
 
   const handler = getHelloHandler({
