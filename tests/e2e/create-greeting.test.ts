1956748cd9282796fcb6c11e466be721cc19faed
Alex Cristea <alexcristea@me.com>
2022-09-01 17:47:52 +0300

    Setup e2e tests

---
+++ tests/e2e/create-greeting.test.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config'
+
+import { Method, callEndpoint } from './shared/callEndpoint'
+
+describe('postGreeting should', () => {
+  test('create new greeting', async () => {
+    const body = {
+      from: 'from@example.com',
+      to: 'to@example.com',
+      message: 'hi!',
+    }
+
+    const result = await callEndpoint('greetings', Method.Post, body)
+
+    expect(new Date(result.body.createdOn)).toBeInstanceOf(Date)
+    expect(new Date(result.body.modifiedOn)).toBeInstanceOf(Date)
+
+    expect(result.body).toMatchObject({
+      id: expect.any(String),
+      from: 'from@example.com',
+      to: 'to@example.com',
+      message: 'hi!',
+      createdOn: expect.any(String),
+      modifiedOn: expect.any(String),
+    })
+  })
+})
---
+++ tests/e2e/shared/callEndpoint.ts
@@ -0,0 +1,17 @@
+import { config } from '../../../src/main/config'
+import supertest from 'supertest'
+import { v4 } from 'uuid'
+
+const { HOSTNAME: host, PORT: port } = config
+
+export enum Method {
+  Post = 'post',
+  Get = 'get',
+  Patch = 'patch',
+  Put = 'put',
+  Delete = 'delete',
+}
+
+export const callEndpoint = (url: string, method: Method, payload: object = {}) => {
+  return supertest.agent(`${host}:${port}/`)[method](url).send(payload).set('trace-id', v4()).redirects(0)
+}
