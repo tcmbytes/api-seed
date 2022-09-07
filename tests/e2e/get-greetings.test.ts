f15c18fd0e785233cd3c204c57bc9205811d897d
Alex Cristea <alexcristea@me.com>
2022-09-07 09:45:40 +0300

    Add e2e test for getting all greetings

---
+++ tests/e2e/get-greetings.test.ts
@@ -0,0 +1,24 @@
+import { callEndpoint, Method } from './shared/callEndpoint'
+import { greetingsClient } from './shared/greetingsClient'
+
+describe('GET /greetings should', () => {
+  test('return 200 and an empty array when there are no greetings in the list', async () => {
+    const result = await callEndpoint('greetings', Method.Get)
+
+    expect(result.status).toStrictEqual(200)
+    expect(result.body).toMatchObject([])
+  })
+
+  test('return 200 and the list off all greetings', async () => {
+    const firstGreeting = await greetingsClient.create()
+    const secondGreeting = await greetingsClient.create()
+
+    const result = await callEndpoint('greetings', Method.Get)
+
+    expect(result.status).toStrictEqual(200)
+    expect(result.body).toMatchObject([firstGreeting.data, secondGreeting.data])
+
+    await greetingsClient.removeById(firstGreeting.data.id)
+    await greetingsClient.removeById(secondGreeting.data.id)
+  })
+})
