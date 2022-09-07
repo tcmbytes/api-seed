89200c10e72fec22fcc7be1c2b286d34e0b625c9
Alex Cristea <alexcristea@me.com>
2022-09-07 09:28:50 +0300

    Add e2e test for updating a greeting

---
+++ tests/e2e/update-greetings.test.ts
@@ -0,0 +1,41 @@
+import { callEndpoint, Method } from './shared/callEndpoint'
+import { greetingsClient } from './shared/greetingsClient'
+
+describe('PUT /greeting/:greetingID should', () => {
+  test('return 404 Not found when the greeting by the provided ID doesnt exist', async () => {
+    const greetingID = 'greetingId'
+
+    const result = await callEndpoint(`greetings/${greetingID}`, Method.Put)
+
+    expect(result.status).toStrictEqual(404)
+    expect(result.body).toMatchObject({
+      message: `Greeting with id '${greetingID}' not found.`,
+    })
+  })
+
+  test('return 200 when the greeting is successfully updated', async () => {
+    const response = await greetingsClient.create()
+    const greetingID = response.data.id
+
+    const request = {
+      message: 'Update message',
+    }
+    const result = await callEndpoint(`greetings/${greetingID}`, Method.Put, request)
+
+    expect(result.status).toStrictEqual(200)
+
+    expect(new Date(result.body.createdOn)).toBeInstanceOf(Date)
+    expect(new Date(result.body.modifiedOn)).toBeInstanceOf(Date)
+
+    expect(result.body).toMatchObject({
+      id: expect.any(String),
+      from: expect.any(String),
+      to: expect.any(String),
+      message: 'Update message',
+      createdOn: expect.any(String),
+      modifiedOn: expect.any(String),
+    })
+
+    await greetingsClient.removeById(greetingID)
+  })
+})
