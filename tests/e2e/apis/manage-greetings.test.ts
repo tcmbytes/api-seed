51d07d7cc3001bd255709ffe9472afaf42c7d350
Alex Cristea <alexcristea@me.com>
2023-02-13 10:50:51 +0200

    Refactor all e2e tests into a single flow e2e test

---
+++ tests/e2e/apis/manage-greetings.test.ts
@@ -0,0 +1,54 @@
+import { Response } from 'supertest'
+import { callEndpoint, Method } from '../shared'
+import { Greeting } from '../types'
+
+describe('Manage greetings flow', () => {
+  let response: Response
+  let greeting: Greeting
+
+  test('successfully creates, gets, edits and deletes greetings', async () => {
+    response = await callEndpoint(Method.POST, '/greetings', {
+      from: 'from@example.com',
+      to: 'to@example.com',
+      message: 'hi!',
+    })
+    greeting = response.body
+
+    expect(response.status).toStrictEqual(201)
+    expect(greeting).toStrictEqual({
+      id: expect.any(String),
+      from: 'from@example.com',
+      to: 'to@example.com',
+      message: 'hi!',
+      createdOn: expect.any(String),
+      modifiedOn: expect.any(String),
+    })
+
+    response = await callEndpoint(Method.GET, '/greetings')
+
+    expect(response.status).toStrictEqual(200)
+    expect(response.body).toStrictEqual([greeting])
+
+    response = await callEndpoint(Method.GET, `/greetings/${greeting.id}`)
+
+    expect(response.status).toStrictEqual(200)
+    expect(response.body).toStrictEqual(greeting)
+
+    response = await callEndpoint(Method.PUT, `/greetings/${greeting.id}`, {
+      message: 'Update message',
+    })
+
+    expect(response.status).toStrictEqual(200)
+    expect(response.body.modifiedOn).not.toEqual(response.body.createdOn)
+    expect(response.body).toStrictEqual({
+      ...greeting,
+      message: 'Update message',
+      modifiedOn: expect.any(String),
+    })
+
+    response = await callEndpoint(Method.DELETE, `/greetings/${greeting.id}`)
+
+    expect(response.status).toStrictEqual(204)
+    expect(response.body).toStrictEqual({})
+  })
+})
